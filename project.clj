4c412c4a567d82d4d1d970109df9d76cfcc205fe
piotr-yuxuan <pierre2b@adikteev.com>
2017-03-14 12:33:06 +0100

    Format project.clj

--- project.clj
+++ project.clj
@@ -3,47 +3,28 @@
                  [org.clojure/clojurescript "1.9.229"]
                  [reagent "0.6.0"]
                  [re-frame "0.9.2"]]
-
   :plugins [[lein-cljsbuild "1.1.4"]]
-
   :min-lein-version "2.5.3"
-
   :source-paths ["src/clj"]
-
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
-
   :figwheel {:css-dirs ["resources/public/css"]}
-
-  :profiles
-  {:dev
-   {:dependencies [[binaryage/devtools "0.8.2"]]
-
-    :plugins      [[lein-figwheel "0.5.9"]]
-    }}
-
-  :cljsbuild
-  {:builds
-   [{:id           "dev"
-     :source-paths ["src/cljs"]
-     :figwheel     {:on-jsload "sample-dirac-re-frame.core/mount-root"}
-     :compiler     {:main                 sample-dirac-re-frame.core
-                    :output-to            "resources/public/js/compiled/app.js"
-                    :output-dir           "resources/public/js/compiled/out"
-                    :asset-path           "js/compiled/out"
-                    :source-map-timestamp true
-                    :preloads             [devtools.preload]
-                    :external-config      {:devtools/config {:features-to-install :all}}
-                    }}
-
-    {:id           "min"
-     :source-paths ["src/cljs"]
-     :compiler     {:main            sample-dirac-re-frame.core
-                    :output-to       "resources/public/js/compiled/app.js"
-                    :optimizations   :advanced
-                    :closure-defines {goog.DEBUG false}
-                    :pretty-print    false}}
-
-
-    ]}
-
-  )
+  :profiles {:dev
+             {:dependencies [[binaryage/devtools "0.8.2"]]
+              :plugins [[lein-figwheel "0.5.9"]]}}
+  :cljsbuild {:builds [{:id "dev"
+                        :source-paths ["src/cljs"]
+                        :figwheel {:on-jsload "sample-dirac-re-frame.core/mount-root"}
+                        :compiler {:main sample-dirac-re-frame.core
+                                   :output-to "resources/public/js/compiled/app.js"
+                                   :output-dir "resources/public/js/compiled/out"
+                                   :asset-path "js/compiled/out"
+                                   :source-map-timestamp true
+                                   :preloads [devtools.preload]
+                                   :external-config {:devtools/config {:features-to-install :all}}}}
+                       {:id "min"
+                        :source-paths ["src/cljs"]
+                        :compiler {:main sample-dirac-re-frame.core
+                                   :output-to "resources/public/js/compiled/app.js"
+                                   :optimizations :advanced
+                                   :closure-defines {goog.DEBUG false}
+                                   :pretty-print false}}]})
