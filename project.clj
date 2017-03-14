c9b23cd9f95c68cac268711d69298f3badcfa21f
piotr-yuxuan <pierre2b@adikteev.com>
2017-03-14 12:36:22 +0100

    Update versions with lein-ancient

--- project.clj
+++ project.clj
@@ -1,18 +1,18 @@
 (defproject sample-dirac-re-frame "0.1.0-SNAPSHOT"
   :license {:name "GNU GPL v3+"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.229"]
-                 [reagent "0.6.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [org.clojure/clojurescript "1.9.495"]
+                 [reagent "0.6.1"]
                  [re-frame "0.9.2"]]
-  :plugins [[lein-cljsbuild "1.1.4"]
+  :plugins [[lein-cljsbuild "1.1.6-SNAPSHOT"]
             [lein-ancient "0.6.10"]]
   :min-lein-version "2.5.3"
   :source-paths ["src/clj"]
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
   :figwheel {:css-dirs ["resources/public/css"]}
   :profiles {:dev
-             {:dependencies [[binaryage/devtools "0.8.2"]]
+             {:dependencies [[binaryage/devtools "0.9.2"]]
               :plugins [[lein-figwheel "0.5.9"]]}}
   :cljsbuild {:builds [{:id "dev"
                         :source-paths ["src/cljs"]
